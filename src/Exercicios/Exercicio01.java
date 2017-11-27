5dd9b223218eb1f646d632de3d973f017ca17394
Leandro Cesar de Almeida <llmanutencao@bol.com.br>
2017-11-27 12:02:21 -0200

    exercicio 01

--- src/Exercicios/Exercicio01.java
+++ src/Exercicios/Exercicio01.java
@@ -5,10 +5,28 @@
  */
 package Exercicios;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Leandro
  */
 public class Exercicio01 {
     
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int num1, num2;
+        
+        System.out.println("Digite o primeiro numero: ");
+        num1 = scan.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        num2 = scan.nextInt();
+        
+        if(num1 > num2){
+            System.out.println("Maior numero: " + num1);
+        }else{
+            System.out.println("Maior numero: " + num2);
+        }
+    }
 }
